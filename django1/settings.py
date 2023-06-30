dd55e29968da49eba12be03a0ac8c95cff10f613
Iuri <iis_@outlook.com>
2023-06-30 00:35:03 -0300

    Modificando ALLOWED HOSTS

--- django1/settings.py
+++ django1/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',
+                 'https://web-production-bb4c.up.railway.app',]
 
 # Application definition
 
