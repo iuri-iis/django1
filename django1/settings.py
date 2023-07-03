ab23a43a2a862ebed486c1a2708d1e2ac723a190
Iuri <iis_@outlook.com>
2023-07-03 00:13:40 -0300

    Removendo executável

--- django1/settings.py
+++ django1/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-bsxca2d87ua2@@spr5vs=5d%$!g%#ui&(z=(_&2ni69trk@-iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-#DEBUG = True
+#DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['*',
@@ -80,11 +81,11 @@ WSGI_APPLICATION = 'django1.wsgi.application'
 #Database
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    #'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
@@ -125,8 +126,7 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 CSRF_TRUSTED_ORIGINS = ['https://django1-production.up.railway.app',
-                        'http://django1-production.up.railway.app',
-                        'django1-production.up.railway.app',]
+                        'http://django1-production.up.railway.app',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
